2667425a769278156f0cdceb3cdfa5291d4aa48c
LucasP <lucas-l-h-p@hotmail.dk>
2022-06-30 21:43:09 +0200

    First CSharp commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! "); //WriteLine will insert a new line at the end of the output each time the method is used 
+            Console.WriteLine("I am Learning C#");
+            Console.Write("C# Can do math. For example is 3 + 3 = "); //Write will not insert a new line at the end of the output each time the method is used
+            Console.WriteLine(3 + 3);
+        }
+    }
+}
