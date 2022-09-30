d44f4ab0657210e2bc01f1d23bc95d4236c59be8
LucasP <lucas-l-h-p@hotmail.dk>
2022-09-30 08:22:47 +0200

    3 lecture files

---
+++ 3-Lektion/SortingCars/SortingCars/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace KG3;
+class Car : IComparable
+{
+    public string Make { get; private set; }
+    public string Model { get; private set; }
+    public decimal Price { get; private set; }
+    public Car(string make, string model, decimal price)
+    {
+        Make = make;
+        Model = model;
+        Price = price;
+    }
+
+    public int CompareTo(object? obj)
+    {
+        
+        return Price.CompareTo(((Car)obj).Price);
+    }
+}
---
+++ 3-Lektion/SortingCars/SortingCars/Program.cs
@@ -0,0 +1,20 @@
+﻿using KG3;
+
+
+static void Main()
+{
+    List<Car> cars = new List<Car>()
+    {
+        new Car("Skoda", "Fabia", 50000m),
+        new Car("Skoda", "Octavia", 60000m),
+        new Car("Fiat", "500", 12345m),
+        new Car("Ford", "Mustang", 9000000m),
+        new Car("Ford", "Mustang", 9000001m)
+        };
+    cars.Sort();
+    Console.WriteLine("Sorted by price");
+    foreach (Car car in cars)
+    {
+        Console.WriteLine($" {car.Make} {car.Model} {car.Price}");
+    }
+}
